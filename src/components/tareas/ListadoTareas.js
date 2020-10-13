8013fc6b1f4adb1e8edf11bab3ce76ee3cc99197
jplozada <juxnpx@gmail.com>
2020-10-12 22:30:32 -0400

    button Editar, Eliminar, EliminarProyecto added to the form

--- src/components/tareas/ListadoTareas.js
+++ src/components/tareas/ListadoTareas.js
@@ -23,6 +23,10 @@ const ListadoTareas = () => {
                     ))
                 }
             </ul>
+            <button
+                type="button"
+                className="btn btn-eliminar"
+            >Eliminar Proyecto &times;</button>
         </Fragment>
     );
 }
