29da5f5922779d46c101250a1ca7038ff341ed31
jplozada <juxnpx@gmail.com>
2020-10-13 10:45:32 -0400

    Condition to show formulario on sidebar

--- src/App.js
+++ src/App.js
@@ -2,17 +2,22 @@ import React from 'react';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Login from './components/auth/Login';
 import NuevaCuenta from './components/auth/NuevaCuenta';
+import Proyecto from './components/proyectos/Proyecto';
 import Proyectos from './components/proyectos/Proyectos';
+
+import ProyectoState from './context/proyectos/proyectoState';
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/" component={Login} />
-        <Route exact path="/nueva-cuenta" component={NuevaCuenta} />
-        <Route exact path="/proyectos" component={Proyectos} />
-      </Switch>
-    </Router>
+    <ProyectoState>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Login} />
+          <Route exact path="/nueva-cuenta" component={NuevaCuenta} />
+          <Route exact path="/proyectos" component={Proyectos} />
+        </Switch>
+      </Router>
+    </ProyectoState>
   );
 }
 
