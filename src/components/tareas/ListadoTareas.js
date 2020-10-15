78cd4363f9d4884ed3cdc9b02f75afaaa2d55b99
jplozada <juxnpx@gmail.com>
2020-10-15 11:41:57 -0400

    Showing proyect form if a proyect is selected

--- src/components/tareas/ListadoTareas.js
+++ src/components/tareas/ListadoTareas.js
@@ -9,7 +9,7 @@ const ListadoTareas = () => {
     const { proyecto } = proyectosContext;
 
     // Si no hay proyecto seleccionado
-    if(!proyecto) return <h2>Selecciona un proyecto</h2>
+    if(!proyecto) return <h2>Selecciona un proyecto</h2>;
 
     // Array destructuring para extraer el proyecto actual
     const [proyectoActual] = proyecto; 
