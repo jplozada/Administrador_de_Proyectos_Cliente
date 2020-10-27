229d14c99f8e04f6e07bf13687afe02c22a01492
jplozada <juxnpx@gmail.com>
2020-10-27 00:50:55 -0400

    Login form validation

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,7 +1,16 @@
-import React, {useState} from 'react';
+import React, {useState, useContext} from 'react';
 import {Link} from 'react-router-dom';
+import AlertaContext from '../../context/alertas/alertaContext';
+import AuthContext from '../../context/autenticacion/authContext';
 
 const Login = () => {
+
+    // Extraer los valores del context
+    const alertaContext  = useContext(AlertaContext);
+    const { alerta, mostrarAlerta } = alertaContext;
+
+    const authContext = useContext(AuthContext);
+    const { mensaje, autenticado, iniciarSesion } = authContext;
 
     // State para iniciar sesion
     const [usuario, guardarUsuario] = useState({
@@ -24,13 +33,24 @@ const Login = () => {
         e.preventDefault();
 
         // Validar que no haya campos vacios
+        if(email.trim() === '' || password.trim() === ''){
+            mostrarAlerta('Todos los campos son obligatorios', 'alerta-error');
+        }
 
         // Pasarlo al action
+        iniciarSesion({email, password});
 
     }
 
     return (  
         <div className="form-usuario">
+            { alerta ? 
+                (
+                    <div className={`alerta ${alerta.categoria}`}>{alerta.msg}</div> 
+                ) 
+                : 
+                null 
+            }
             <div className="contenedor-form sombra-dark">
                 <h1>Iniciar Sesión</h1>
                 <form
