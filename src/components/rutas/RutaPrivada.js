150ce321daf91a9b9fc33be9647fcc480fc05b57
jplozada <juxnpx@gmail.com>
2020-10-28 14:36:00 -0400

    Better logged in user experience when Proyectos page is refreshed

--- src/components/rutas/RutaPrivada.js
+++ src/components/rutas/RutaPrivada.js
@@ -5,11 +5,15 @@ import AuthContext from '../../context/autenticacion/authContext';
 const RutaPrivada = ({component: Component, ...props}) => {
 
     const authContext = useContext(AuthContext);
-    const {autenticado} = authContext;
+    const {autenticado, cargando, usuarioAutenticado} = authContext;
+
+    useEffect(() => {
+        usuarioAutenticado();
+    }, [])
 
     return (
         <Route {...props} render = {
-            props => !autenticado ? (
+            props => !autenticado && !cargando ? (
             <Redirect to="/" />
         ) : (
             <Component {...props} />
