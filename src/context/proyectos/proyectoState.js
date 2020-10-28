f58adf1dbb1a300432e15fd0fe49840fb224628e
jplozada <juxnpx@gmail.com>
2020-10-28 17:26:13 -0400

    Delete Proyecto in the database

--- src/context/proyectos/proyectoState.js
+++ src/context/proyectos/proyectoState.js
@@ -77,11 +77,16 @@ const ProyectoState = props => {
     }
 
     // Eliminar un proyecto
-    const eliminarProyecto = proyectoId => {
-        dispatch({
-            type: ELIMINAR_PROYECTO,
-            payload: proyectoId
-        })
+    const eliminarProyecto = async proyectoId => {
+        try {
+            await clienteAxios.delete(`/api/proyectos/${proyectoId}`);
+            dispatch({
+                type: ELIMINAR_PROYECTO,
+                payload: proyectoId
+            })
+        } catch (error) {
+            console.log(error);
+        }
     }
 
     return (
