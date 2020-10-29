67afcbb5bee475994510e6116aa9b75068f06cca
jplozada <juxnpx@gmail.com>
2020-10-29 11:12:54 -0400

    Tareas DELETE request

--- src/components/tareas/Tarea.js
+++ src/components/tareas/Tarea.js
@@ -17,7 +17,7 @@ const Tarea = ({tarea}) => {
     
     // Funcion que se ejecuta cuando el usuario preisona el btn de eliminar tarea
     const tareaEliminar = id => {
-        eliminarTarea(id);
+        eliminarTarea(id, proyectoActual._id);
         obtenerTareas(proyectoActual.id);
     }
 
@@ -68,7 +68,7 @@ const Tarea = ({tarea}) => {
                 <button
                     type="button"
                     className="btn btn-secundario"
-                    onClick={() => tareaEliminar(tarea.id)}
+                    onClick={() => tareaEliminar(tarea._id)}
                 >Eliminar</button>
             </div>
         </li>
