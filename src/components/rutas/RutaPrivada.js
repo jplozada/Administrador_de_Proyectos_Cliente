9152ecaf436e8d1770e317030275222c9143d378
jplozada <juxnpx@gmail.com>
2020-10-29 17:04:17 -0400

    React warnings fixed

--- src/components/rutas/RutaPrivada.js
+++ src/components/rutas/RutaPrivada.js
@@ -9,7 +9,10 @@ const RutaPrivada = ({component: Component, ...props}) => {
 
     useEffect(() => {
         usuarioAutenticado();
-    }, [])
+
+        // eslint-disable-next-line
+        // if there is a loop in the app
+    }, [usuarioAutenticado])
 
     return (
         <Route {...props} render = {
