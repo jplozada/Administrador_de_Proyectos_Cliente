e7f2366a6e57617e02dfa8c6baab8cea20707ab5
jplozada <juxnpx@gmail.com>
2020-10-29 00:36:32 -0400

    Tareas POST request

--- src/components/tareas/FormTarea.js
+++ src/components/tareas/FormTarea.js
@@ -57,8 +57,7 @@ const FormTarea = () => {
         // Si es edicion o si es nueva tarea
         if(tareaseleccionada === null) {
             // Agregar la nueva tarea al state de tareas
-            tarea.proyectoId = proyectoActual.id;
-            tarea.estado = false;
+            tarea.proyecto = proyectoActual._id;
             agregarTarea(tarea);
         }else{
             // Actualizar tarea existente
